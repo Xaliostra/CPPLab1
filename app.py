bd64a8d28e56dcec51f6c4e535e846869c93e630
Xaliostra <145377372+Xaliostra@users.noreply.github.com>
2025-03-19 14:56:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import time
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+#a
 # Загрузка переменных окружения
 load_dotenv()
 
